49a2938c7ff6d005c4e1c15a080d1772710bf1f0
Tom OLIVIER <olivier.tom.35@gmail.com>
2017-12-13 19:04:08 +0100

    Added WS

--- app/src/main/java/fr/mds/mydogtrackermanager/retrofit/DogTrackerService.java
+++ app/src/main/java/fr/mds/mydogtrackermanager/retrofit/DogTrackerService.java
@@ -8,11 +8,39 @@ import retrofit2.http.Query;
 
 public interface DogTrackerService {
 
-    @GET("/MYURL")
-    Call<DataContainer> myMethod(@Query("parameter") int parameterValue);
+    String URL = "http://dogtracker.epizy.com/ws.php";
 
-    @POST("/URL")
+    @POST(URL)
     @FormUrlEncoded
+    Call<DataContainer> add_user(@Query("name") String name, @Query("lastname") String lastname, @Query("password") String password, @Query("email") String email);
+
+    @POST(URL)
+    @FormUrlEncoded
+    Call<DataContainer> add_position(@Query("spy_id") String spyId, @Query("longitude") String longitude, @Query("latitude") String latitude);
+
+    @POST(URL)
+    @FormUrlEncoded
+    Call<DataContainer> add_spy(@Query("spy_name") String spy_name);
+
+    @POST(URL)
+    @FormUrlEncoded
+    Call<DataContainer> remove_spy(@Query("spy_name") String spy_name);
+
+    @POST(URL)
+    @FormUrlEncoded
+    Call<DataContainer> find_spy(@Query("spy_name") String spy_name);
+
+    @POST(URL)
+    @FormUrlEncoded
+    Call<DataContainer> update_name_spy(@Query("spy_id") String spy_id, @Query("spy_name") String spy_name);
+
+    @GET(URL)
+    Call<DataContainer> retrieve_last_pos_spies(@Query("user_id") String user_id);
+
+    @GET(URL)
+    Call<DataContainer> retrieve_all_pos_spy(@Query("spy_id") String spy_id, @Query("user_id") String user_id);
+
+    @GET(URL)
     Call<DataContainer> checkPassword(@Query("user_id") String id, @Query("password") String password);
 
 }
